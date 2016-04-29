be3cc7a0a2f9fd0f9ddaf0b70c7f9d1d162b79e0
Oleg Belozeorov <upwawet@gmail.com>
2016-04-29 19:57:47 +0500

    use haskell-indentation-mode

--- GNUEmacs.dot/init.d/haskell-mode.el
+++ GNUEmacs.dot/init.d/haskell-mode.el
@@ -1,8 +1,3 @@
-;(add-to-list 'load-path "/opt/emacs-cvs/share/emacs/site-lisp/haskell-mode")
-;(load "haskell-site-file")
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-(add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
+(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
 
 (init:provide 'haskell-mode)
-
